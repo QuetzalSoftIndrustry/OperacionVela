9210ec151aaa870b9109e8af0e5235905f815e22
Edson Sotomayor <edsonio-100@hotmail.com>
2020-12-13 22:57:26 -0600

    Menu principal

---
+++ Scripts/Interfaz.gd
@@ -0,0 +1,64 @@
+extends CanvasLayer
+
+export (PackedScene) var scn_game
+
+func _on_BotonOpciones_pressed():
+	$FondoMenu.hide()
+	$BotonOpciones.hide()
+	$BotonStart.hide()
+	$BotonSalir.hide()
+	$Salir.hide()
+	$Titulo.hide()
+	$Opciones.hide()
+	$FondoOpciones.show()
+	$BotonAtras.show()
+	$Atras.show()
+	$BotonComoJugar.show()
+	$Sonido.show()
+	$BotonSonido.show()
+
+func _on_BotonAtras_pressed():
+	$FondoMenu.show()
+	$BotonOpciones.show()
+	$BotonStart.show()
+	$BotonSalir.show()
+	$Salir.show()
+	$Jugar.show()
+	$Titulo.show()
+	$Opciones.show()
+	$FondoOpciones.hide()
+	$BotonAtras.hide()
+	$Atras.hide()
+	$BotonComoJugar.hide()
+	$Sonido.hide()
+	$BotonSonido.hide()
+	$ColorRect.hide()
+	$Controles.hide()
+	$Vida.hide()
+	$Basura.hide()
+	$Amigos.hide()
+
+func _on_BotonComoJugar_pressed():
+	$BotonComoJugar.hide()
+	$Sonido.hide()
+	$BotonSonido.hide()
+	$ColorRect.show()
+	$Controles.show()
+	$Vida.show()
+	$Basura.show()
+	$Amigos.show()
+
+func _on_BotonStart_pressed():
+	$FondoMenu.hide()
+	$FondoMenu/TextureRect.hide()
+	$BotonOpciones.hide()
+	$BotonStart.hide()
+	$BotonSalir.hide()
+	$Salir.hide()
+	$Titulo.hide()
+	$Jugar.hide()
+	$Opciones.hide()
+	get_tree().change_scene_to(scn_game)
+
+func _on_BotonSalir_pressed():
+	get_tree().quit()
